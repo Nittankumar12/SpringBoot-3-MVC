ebfa121806f12b87f0454802ad4a139ca61eacaf
nittan <nitin01kumar14@gmail.com>
2024-03-05 23:27:46 +0530

    first commit

---
+++ src/main/java/com/Railworld/SpringBootWeb1/HomeController.java
@@ -0,0 +1,46 @@
+package com.Railworld.SpringBootWeb1;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class HomeController {
+
+    @RequestMapping
+    public String home(){
+        System.out.println("Home method called");
+        return "index.jsp";
+    }
+
+//    @RequestMapping("add")
+//    public String add(HttpServletRequest request, HttpSession session){
+//        int num1 = Integer.parseInt(request.getParameter("num1"));
+//        int num2 = Integer.parseInt(request.getParameter("num2"));
+//        int result = num1 + num2;
+////        System.out.println(result); // prints data on console but if we want to print data on the page we use HttpSession
+//        session.setAttribute("r1", result);
+//        return "result.jsp";
+//    }
+
+//     @RequestMapping("add")
+//    public String add(int num1, int num2, HttpSession session){ // its basically matching the num1 and num2 names in index.jsp
+//      public String add(@RequestParam("num1") int number1, int num2, HttpSession session){ // if we have different names then we can use @RequestParam("originalName")
+//        int result = number1 + num2;
+//        session.setAttribute("r1", result);
+//        return "result.jsp";
+//    }
+
+     @RequestMapping("add")
+    public String add(int num1, int num2, Model model){
+
+        int result = num1 + num2;
+        model.addAttribute("r1",result);
+        return "result.jsp";
+    }
+}
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,15 @@
+<%@page language="java"%>
+
+<html>
+        <head>
+        <link rel= "stylesheet" type="text/css" href="style.css">
+        </head>
+<body>
+<%--   <h2>Result is:  <%= session.getAttribute("r1") %> </h2> --%>
+<%--  or we can do like this:  --%>
+<h2>Result is: ${r1} </h2>
+
+
+
+</body>
+</html>
